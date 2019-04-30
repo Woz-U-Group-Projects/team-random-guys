842d55db756a863bbf7dd1e45153b16c9dbf95bc
Justin Trombley <jtrombley96@gmail.com>
2019-04-30 01:04:16 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TimeSelectionModule } from './modules/time-selection/time-selection.module';
 
-import { AppComponent } from './app.component';
-
-
 @NgModule({
   declarations: [
     AppComponent,
